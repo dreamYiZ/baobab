e416289b285aec6faf8e023766c66b88e4b94bf1
Rob <dreamYiZ@outlook.com>
2021-12-09 23:12:37 +0800

    -

--- i18n/index.js
+++ i18n/index.js
@@ -2,14 +2,18 @@ import * as cn from "./cn";
 
 let language;
 let i18n = {
-    cn
-}
-const i18nWord = (word)=>{
-    const LanguageList  = ['cn']
-    if( language && LanguageList.includes( language)){
-        return i18n[language].get(word)
-    }
-    return word
-}
+  cn,
+};
+const i18nWord = (word) => {
+  const LanguageList = ["cn"];
+  if (language && LanguageList.includes(language)) {
+    return i18n[language].get(word);
+  }
+  return word;
+};
 
-export { i18nWord, language }
+const setLanguage = (l) => {
+  language = l;
+};
+
+export { i18nWord, setLanguage };
--- logit.js
+++ logit.js
@@ -1,12 +1,17 @@
-import {i18nWord} from "./i18n";
+import { i18nWord, setLanguage } from "./i18n";
 
-const log = (...x)=>{
-    console.log(...x)
-}
+const log = (...x) => {
+  console.log(...x);
+};
+
+
+
 export default {
-    install: (app, options) => {
-      // Plugin code goes here  
-      log(i18nWord(render), 'app')
-      
-    }
-  }
+  install: (app, options) => {
+    // Plugin code goes here
+    options.language && setLanguage(options.language);
+
+
+    log(i18nWord('render'), "app");
+  },
+};
